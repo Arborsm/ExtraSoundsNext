e6a7d5b96f4abaa87e6154a5e499abbeb8034370
Arborsm <BlueGrayx@qq.com>
2024-10-20 15:23:23 +0800

    fix #35

--- fabric/src/main/java/dev/arbor/extrasoundsnext/mixin/jei/TypedIngredientMixin.java
+++ fabric/src/main/java/dev/arbor/extrasoundsnext/mixin/jei/TypedIngredientMixin.java
@@ -3,23 +3,27 @@ package dev.arbor.extrasoundsnext.mixin.jei;
 import dev.arbor.extrasoundsnext.sounds.SoundManager;
 import dev.arbor.extrasoundsnext.sounds.SoundType;
 import mezz.jei.api.ingredients.ITypedIngredient;
+import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.runtime.IIngredientManager;
+import mezz.jei.library.focus.Focus;
 import mezz.jei.library.ingredients.TypedIngredient;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Optional;
-
-@Mixin(TypedIngredient.class)
+@Mixin(Focus.class)
 @Environment(EnvType.CLIENT)
 public class TypedIngredientMixin {
-    @Inject(method = "deepCopy", at = @At("TAIL"), remap = false)
-    private static <T> void deepCopy(IIngredientManager ingredientManager, ITypedIngredient<T> value, CallbackInfoReturnable<Optional<ITypedIngredient<T>>> cir) {
-        SoundManager.playSound(value.getItemStack().orElse(ItemStack.EMPTY), SoundType.PICKUP);
+    @Inject(method = "createFromApi(Lmezz/jei/api/runtime/IIngredientManager;Lmezz/jei/api/recipe/RecipeIngredientRole;Lmezz/jei/api/ingredients/ITypedIngredient;)Lmezz/jei/library/focus/Focus;", at = @At("RETURN"), remap = false)
+    private static <V> void deepCopy(IIngredientManager ingredientManager, RecipeIngredientRole role, ITypedIngredient<V> typedIngredient, CallbackInfoReturnable<Focus<V>> cir) {
+        @Nullable ITypedIngredient<V> typedIngredientCopy = TypedIngredient.defensivelyCopyTypedIngredientFromApi(ingredientManager, typedIngredient);
+        if (typedIngredientCopy != null) {
+            SoundManager.playSound(typedIngredientCopy.getItemStack().orElse(ItemStack.EMPTY), SoundType.PICKUP);
+        }
     }
 }
--- neoforge/src/main/java/dev/arbor/extrasoundsnext/mixin/jei/TypedIngredientMixin.java
+++ neoforge/src/main/java/dev/arbor/extrasoundsnext/mixin/jei/TypedIngredientMixin.java
@@ -1,25 +1,29 @@
 package dev.arbor.extrasoundsnext.mixin.jei;
 
 import mezz.jei.api.ingredients.ITypedIngredient;
+import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.runtime.IIngredientManager;
+import mezz.jei.library.focus.Focus;
 import mezz.jei.library.ingredients.TypedIngredient;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import dev.arbor.extrasoundsnext.sounds.SoundManager;
 import dev.arbor.extrasoundsnext.sounds.SoundType;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Optional;
-
-@Mixin(TypedIngredient.class)
+@Mixin(Focus.class)
 @OnlyIn(Dist.CLIENT)
 public class TypedIngredientMixin {
-    @Inject(method = "deepCopy", at = @At("TAIL"), remap = false)
-    private static <T> void deepCopy(IIngredientManager ingredientManager, ITypedIngredient<T> value, CallbackInfoReturnable<Optional<ITypedIngredient<T>>> cir) {
-        SoundManager.playSound(value.getItemStack().orElse(ItemStack.EMPTY), SoundType.PICKUP);
+    @Inject(method = "createFromApi(Lmezz/jei/api/runtime/IIngredientManager;Lmezz/jei/api/recipe/RecipeIngredientRole;Lmezz/jei/api/ingredients/ITypedIngredient;)Lmezz/jei/library/focus/Focus;", at = @At("RETURN"), remap = false)
+    private static <V> void deepCopy(IIngredientManager ingredientManager, RecipeIngredientRole role, ITypedIngredient<V> typedIngredient, CallbackInfoReturnable<Focus<V>> cir) {
+        @Nullable ITypedIngredient<V> typedIngredientCopy = TypedIngredient.defensivelyCopyTypedIngredientFromApi(ingredientManager, typedIngredient);
+        if (typedIngredientCopy != null) {
+            SoundManager.playSound(typedIngredientCopy.getItemStack().orElse(ItemStack.EMPTY), SoundType.PICKUP);
+        }
     }
 }
